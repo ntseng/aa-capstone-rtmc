9f66032c34d55245ea15e9dded81d0fc6d8675f6
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-15 08:33:14 -0800

    removed faker from seed data

--- backend/db/seeders/20201030163017-demo-user.js
+++ backend/db/seeders/20201030163017-demo-user.js
@@ -1,5 +1,4 @@
 'use strict';
-const faker = require("faker");
 const bcrypt = require("bcryptjs");
 
 module.exports = {
@@ -9,24 +8,14 @@ module.exports = {
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password'),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync(faker.internet.password()),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync(faker.internet.password()),
-      },
+      }
     ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+      username: { [Op.in]: ['Demo-lition'] }
     }, {});
   }
 };
