d819d2cae11d95e805270bd6ea2d1480f292bfda
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-15 11:01:03 -0800

    Make repo readme Fixes #3

---
+++ README.md
@@ -0,0 +1,19 @@
+#
+This student project is
+
+## Setup Instructions
+
+## Time Budget
+Time allotted: 80 person*hours
+Time to completion:
+
+## Links
+- [Live Site](https://aa-capstone.herokuapp.com/)
+- [Scorecard](https://docs.google.com/spreadsheets/d/1q0Si8iOlSxLZ8mbcJQUrQwm-4LoPyUtGMN8frIeQGcA/)
+- [DB Schema](https://github.com/ntseng/aa-capstone/wiki/DB-Schema)
+- [Feature List](https://github.com/ntseng/aa-capstone/wiki/Feature-List)
+- [Technology List](https://github.com/ntseng/aa-capstone/wiki/Technology-List)
+- [User Stories](https://github.com/ntseng/aa-capstone/wiki/User-Stories)
+- [Wireframes](https://github.com/ntseng/aa-capstone/wiki/Wireframes)
+
+## Challenges
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,38 +3,38 @@ import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import SignupFormPage from './components/SignupFormPage';
 import LoginFormPage from "./components/LoginFormPage";
-import * as sessionActions from './store/session';
+import { restoreUser } from './store/session';
 import Navigation from './components/Navigation';
 // import { Modal } from './components/Modal';
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-//   const [showModal, setShowModal] = useState(false);
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
+	const dispatch = useDispatch();
+	const [isLoaded, setIsLoaded] = useState(false);
+	//   const [showModal, setShowModal] = useState(false);
+	useEffect(() => {
+		dispatch(restoreUser()).then(() => setIsLoaded(true));
+	}, [dispatch]);
 
-  return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {/* <button onClick={() => setShowModal(true)}>Modal</button> */}
-      {/* {showModal && (
+	return (
+		<>
+			<Navigation isLoaded={isLoaded} />
+			{/* <button onClick={() => setShowModal(true)}>Modal</button> */}
+			{/* {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <h1>Hello I am a Modal</h1>
         </Modal>
       )} */}
-      {isLoaded && (
-        <Switch>
-          <Route path="/login" >
-            <LoginFormPage />
-          </Route>
-          <Route path='/signup'>
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
-    </>
-  );
+			{isLoaded && (
+				<Switch>
+					<Route path="/login" >
+						<LoginFormPage />
+					</Route>
+					<Route path='/signup'>
+						<SignupFormPage />
+					</Route>
+				</Switch>
+			)}
+		</>
+	);
 }
 
 export default App;
