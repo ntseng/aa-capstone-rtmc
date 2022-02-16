a82dd114c77cae33cc070ea921a248152cc98914
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-16 15:54:26 -0800

    Logged In Footer: elements Fixes #33

---
+++ frontend/src/components/AppFooter.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function AppFooter() {
+	return (<ul>
+		<Link>Link 1</Link>
+		<Link>Link 2</Link>
+		<Link>Link 3</Link>
+		<Link>Link 4</Link>
+	</ul>)
+}
