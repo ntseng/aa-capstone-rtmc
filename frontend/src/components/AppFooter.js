ef2e3dabb73b2467779b512116bb71323670a3be
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-17 14:19:10 -0800

    Logged In Footer: styling Fixes #34

--- frontend/src/components/AppFooter.js
+++ frontend/src/components/AppFooter.js
@@ -1,10 +1,28 @@
-import { Link } from "react-router-dom";
+import "./css/AppFooter.css";
 
 export default function AppFooter() {
-	return (<ul>
-		<Link>Link 1</Link>
-		<Link>Link 2</Link>
-		<Link>Link 3</Link>
-		<Link>Link 4</Link>
-	</ul>)
+	return (<div id="footer-container">
+		<ul>
+			<li>
+				<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/nathaniel-tseng-14404838/">Developer</a>
+			</li>
+			<li>
+				<a target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone">Repository</a>
+			</li>
+			<li>
+				<a target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone/wiki/DB-Schema">DB Schema</a>
+			</li>
+			<li>
+				<a target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone/wiki/Wireframes">Wireframes</a>
+			</li>
+			<li>
+				<a target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone/wiki/Technology-List">Technology List</a>
+			</li>
+			<li>
+				<a target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone/wiki/User-Stories">User Stories</a>
+			</li>
+
+		</ul>
+		<div>○ 2022 Remember the Milk Clone</div>
+	</div>)
 }
