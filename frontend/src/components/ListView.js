19dec9247ac4420457f10c57aef0145f06b533cc
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-18 09:41:16 -0800

    Lists Navbar: styling Fixes #26

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -1,22 +1,25 @@
 import { useDispatch, useSelector } from "react-redux"
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { trashList } from "../store/lists";
+import "./css/ListView.css";
 
 export default function ListView({ inboxId, ownerId }) {
 	const dispatch = useDispatch();
 	const lists = useSelector(state => state.lists);
-	return (<>
+	return (<div id="list-view-container">
 		{/* TODO #58 create logo */}
-		<Link to="/app/all">All Tasks</Link>
-		{
-			Object.values(lists).map((list, index) => {
-				return (<div key={index}>
-					<Link to={`/app/${list.id}`}>
-						{list.title}
-					</Link>
-					{inboxId === list.id ? (<></>) : (<button onClick={e => dispatch(trashList({ listId: list.id, ownerId, inboxId }))}>Delete</button>)}
-				</div>)
-			})
-		}
-	</>)
+		<ul>
+			<NavLink className="list-view-item" to="/app/all">All Tasks</NavLink>
+			{
+				Object.values(lists).map((list, index) => {
+					return (<div key={index}>
+						<NavLink className="list-view-item" to={`/app/${list.id}`}>
+							{list.title}
+						</NavLink>
+						{inboxId === list.id ? (<></>) : (<button onClick={e => dispatch(trashList({ listId: list.id, ownerId, inboxId }))}>Delete</button>)}
+					</div>)
+				})
+			}
+		</ul>
+	</div>)
 }
