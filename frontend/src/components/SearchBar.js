cff8790089a844d23c5603394d35f1df506ed5b7
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-21 13:37:46 -0800

    search functionality

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,11 +1,14 @@
 import ProfileButton from "./ProfileButton";
+import { useDispatch } from "react-redux";
+import { fetchSearchedTasks } from "../store/tasks";
 import "./css/SearchBar.css";
 
 export default function SearchBar({ user }) {
+	const dispatch = useDispatch();
 	return (<div id="search-nav">
 		<div id="search-div">
 			<i className="fa-solid fa-magnifying-glass" />
-			<input id="search-input" type="text" />
+			<input id="search-input" type="search" onBlur={e => dispatch(fetchSearchedTasks({ ownerId: user.id, searchTerm: e.target.value }))} />
 		</div>
 		<div id="search-nav-actions">
 			<ProfileButton user={user} />
