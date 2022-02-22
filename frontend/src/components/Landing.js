1b5c76c1dace0f8ebb767abc384c24d4da231cdd
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-22 10:37:01 -0800

    landing nav styling Fixes #6

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -9,26 +9,30 @@ export default function Landing() {
 	if (sessionUser) return <Redirect to="/app/all" />;
 	return (<>
 		<ul id="landing-nav-ul">
-			<li>
-				<Link to="/"><img id="nav-logo" src="/images/cow.png" alt="logo" /></Link>
-			</li>
-			<li className="landing-nav-item">
-				<Link to="/login">Login</Link>
-			</li>
-			<li className="landing-nav-item">
-				<Link to="/login">Login</Link>
-			</li>
-			<li className="landing-nav-item">
-				<Link to="/login">Login</Link>
-			</li>
-			<li className="landing-nav-item">
-				<Link to="/signup">Sign up for free</Link>
-			</li>
+			<div id="nav-container">
+				<li className="landing-nav-item">
+					<Link to="/"><img id="nav-logo" src="/images/cow.png" alt="logo" /></Link>
+				</li>
+				<div id="nav-actions-container">
+					<li className="landing-nav-item">
+						<Link className="nav-link" to="/login">Login</Link>
+					</li>
+					<li className="landing-nav-item">
+						<Link className="nav-link" to="/login">Login</Link>
+					</li>
+					<li className="landing-nav-item">
+						<Link className="nav-link" to="/login">Login</Link>
+					</li>
+					<li className="landing-nav-item">
+						<Link id="sign-up-button" to="/signup">Sign up for free</Link>
+					</li>
+				</div>
+			</div>
 		</ul>
 		<header>
 			<div>
-				<h1>The smart to-do app for busy people</h1>
-				<Link to="/signup">Sign Up Free</Link>
+				<h1>The smart to-do app for busy people.</h1>
+				<Link id="call-to-action-button" to="/signup">Sign Up Free</Link>
 			</div>
 			<img id="landing-logo" src="/images/cow.svg" alt="logo" />
 		</header>
