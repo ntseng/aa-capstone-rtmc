4cf6c513310ab9d1193e8f35ec3982fa21b8c1a7
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-22 14:02:46 -0800

    signup styling

--- frontend/src/components/SignupPage.js
+++ frontend/src/components/SignupPage.js
@@ -44,54 +44,55 @@ function SignupPage() {
 				<h2 id="signup-h2">Join millions (?) of people getting more organized and productive!</h2>
 			</div>
 			<div id="right-container">
-				<span>Already have an account?</span>
-				<Link to="/login">Sign in</Link>
-				<h1>Sign up for free.</h1>
-				<form onSubmit={handleSubmit}>
-					<div>
-						<input
-							className={errors.filter(error => error.includes("email") && !error.includes("Username")).length ? "errored-input" : ""}
-							placeholder="Email"
-							type="text"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-						{errors.filter(error => error.includes("email") && !error.includes("Username")).map((error, idx) => <div key={idx} className="error-message">{error}</div>)}
+				<div id="form-container">
+					<div id="swap-div">
+						<span id="swap-text">Already have an account?</span>
+						<Link id="swap-button" to="/login">Log in</Link>
 					</div>
-					<div>
-						<input
-							className={errors.filter(error => error.includes("username") || error.includes("Username")).length ? "errored-input" : ""}
-							placeholder="Username"
-							type="text"
-							value={username}
-							onChange={(e) => setUsername(e.target.value)}
-						/>
-						{errors.filter(error => error.includes("username") || error.includes("Username")).map((error, idx) => <div key={idx} className="error-message">{error}</div>)}
-					</div>
-					<div>
-						<input
-							className={errors.filter(error => error.includes("Password")).length ? "errored-input" : ""}
-							placeholder="Password"
-							type="password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-						{errors.filter(error => error.includes("Password")).map((error, idx) => <div key={idx} className="error-message">{error}</div>)}
-					</div>
-					<div>
-						<input
-							className={errors.filter(error => error.includes("Confirm")).length ? "errored-input" : ""}
-							placeholder="Confirm Password"
-							type="password"
-							value={confirmPassword}
-							onChange={(e) => setConfirmPassword(e.target.value)}
-						/>
-						{errors.filter(error => error.includes("Confirm")).map((error, idx) => <div key={idx} className="error-message">{error}</div>)}
-					</div>
-					<button id="signup-button" type="submit">Sign up!</button>
-				</form>
-				<button onClick={e => dispatch(demoLogin()).then(() => history.push("/app/all"))}>Demo Login</button>
-				<div>The original site has some text here about agreeing to their Terms of Service and Privacy Policies.</div>
+					<form id="auth-form" onSubmit={handleSubmit}>
+						<h3 id="auth-h3">Sign up for free.</h3>
+						<div>
+							<input className={errors.filter(error => error.includes("email") && !error.includes("Username")).length ? "auth-input errored-input" : "auth-input"}
+								placeholder="Email"
+								type="text"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+							{errors.filter(error => error.includes("email") && !error.includes("Username")).map((error, idx) => <div key={idx} className="error-message">{error}</div>)}
+						</div>
+						<div>
+							<input className={errors.filter(error => error.includes("username") || error.includes("Username")).length ? "auth-input errored-input" : "auth-input"}
+								placeholder="Username"
+								type="text"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+							/>
+							{errors.filter(error => error.includes("username") || error.includes("Username")).map((error, idx) => <div key={idx} className="error-message">{error}</div>)}
+						</div>
+						<div>
+							<input className={errors.filter(error => error.includes("Password")).length ? "auth-input errored-input" : "auth-input"}
+								placeholder="Password"
+								type="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+							{errors.filter(error => error.includes("Password")).map((error, idx) => <div key={idx} className="error-message">{error}</div>)}
+						</div>
+						<div>
+							<input className={errors.filter(error => error.includes("Confirm")).length ? "auth-input errored-input" : "auth-input"}
+								placeholder="Confirm Password"
+								type="password"
+								value={confirmPassword}
+								onChange={(e) => setConfirmPassword(e.target.value)}
+							/>
+							{errors.filter(error => error.includes("Confirm")).map((error, idx) => <div key={idx} className="error-message">{error}</div>)}
+						</div>
+						<button className="signup-button" type="submit">Sign up!</button>
+						<hr />
+						<button className="signup-button" onClick={e => { e.preventDefault(); dispatch(demoLogin()).then(() => history.push("/app/all")) }}>Demo Login</button>
+					</form>
+					<div id="tos-text">The original site has some text here about agreeing to their Terms of Service and Privacy Policies.</div>
+				</div>
 			</div>
 		</div>
 	);
