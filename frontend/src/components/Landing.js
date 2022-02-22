bf8022292802263a14d547cdab898f56bd87c070
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-21 16:08:40 -0800

    Home Page: elements Fixes #5

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,9 +1,61 @@
 import { useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import AppFooter from "./AppFooter";
+import "./css/Landing.css";
 
 export default function Landing() {
 	const sessionUser = useSelector(state => state.session.user);
 
 	if (sessionUser) return <Redirect to="/app/all" />;
-	return (<></>)
+	return (<>
+		<ul id="landing-nav-ul">
+			<li>
+				<Link to="/"><img id="nav-logo" src="/images/cow.png" alt="logo" /></Link>
+			</li>
+			<li className="landing-nav-item">
+				<Link to="/login">Login</Link>
+			</li>
+			<li className="landing-nav-item">
+				<Link to="/login">Login</Link>
+			</li>
+			<li className="landing-nav-item">
+				<Link to="/login">Login</Link>
+			</li>
+			<li className="landing-nav-item">
+				<Link to="/signup">Sign up for free</Link>
+			</li>
+		</ul>
+		<header>
+			<div>
+				<h1>The smart to-do app for busy people</h1>
+				<Link to="/signup">Sign Up Free</Link>
+			</div>
+			<img id="landing-logo" src="/images/cow.svg" alt="logo" />
+		</header>
+		<footer>
+			<div>
+				<ul>
+					<li>link</li>
+					<li>link</li>
+					<li>link</li>
+				</ul>
+				<ul>
+					<li>link</li>
+					<li>link</li>
+					<li>link</li>
+				</ul>
+				<ul>
+					<li>link</li>
+					<li>link</li>
+					<li>link</li>
+				</ul>
+				<ul>
+					<li>link</li>
+					<li>link</li>
+					<li>link</li>
+				</ul>
+			</div>
+			<AppFooter />
+		</footer>
+	</>)
 }
