322f49b1ee80c26cb5efa5201ec49601c8a54cd4
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-22 13:04:26 -0800

    Landing footer styling Closes #36

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
-import AppFooter from "./AppFooter";
 import "./css/Landing.css";
 
 export default function Landing() {
@@ -35,32 +34,41 @@ export default function Landing() {
 		<header>
 			<h1 id="landing-h1">The smart to-do app for busy people.</h1>
 			<Link id="call-to-action-button" to="/signup">Sign Up Free</Link>
-			<img id="landing-logo" src="/images/cow.svg" alt="logo" />
+			<img id="landing-img" src="/images/paul-hanaoka-4ZaH0DGGomI-unsplash.jpg" alt="logo" />
 		</header>
 		<footer>
 			<div>
-				<ul>
-					<li>link</li>
-					<li>link</li>
-					<li>link</li>
-				</ul>
-				<ul>
-					<li>link</li>
-					<li>link</li>
-					<li>link</li>
-				</ul>
-				<ul>
-					<li>link</li>
-					<li>link</li>
-					<li>link</li>
-				</ul>
-				<ul>
-					<li>link</li>
-					<li>link</li>
-					<li>link</li>
-				</ul>
+				<div id="footer-container">
+					<img id="landing-logo" src="/images/cow.svg" alt="logo" />
+					<ul>
+						<div className="category-heading">Developer</div>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/nathaniel-tseng-14404838/">LinkedIn</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng">GitHub</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/spreadsheets/d/1oUYEFjFufv3GhipZB3FYK4PMajOvvq6wDjri6ycO6PM/edit#gid=122240886">Scorecard</a></li>
+					</ul>
+					<ul>
+						<div className="category-heading">Product</div>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone">Repository</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone/wiki/DB-Schema">DB Schema</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone/wiki/Wireframes">Wireframes</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone/wiki/Technology-List">Technology List</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://github.com/ntseng/aa-capstone/wiki/User-Stories">User Stories</a></li>
+					</ul>
+					<ul>
+						<div className="category-heading">Credits</div>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://www.rememberthemilk.com/">Original Site</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://game-icons.net/1x1/delapouite/cow.html">Logo/Favicon Credit</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://unsplash.com/@plhnk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Landing Photo</a></li>
+					</ul>
+					<ul>
+						<div className="category-heading">Previous Projects</div>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://aa-group-project-teachables.herokuapp.com/">Teachables</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://aa-soloproject-evernote-clone.herokuapp.com/">Evernote Clone</a></li>
+						<li className="footer-li"><a className="footer-a" target="_blank" rel="noopener noreferrer" href="https://extension-hunt.herokuapp.com/">Extension Hunt</a></li>
+					</ul>
+				</div>
+				<div id="landing-copyright">○ 2022 Remember the Milk Clone</div>
 			</div>
-			<AppFooter />
 		</footer>
 	</>)
 }
